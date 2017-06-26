e65a1d26ee78f0c73bb1c86189f06254057aa971
Pavel Samsonov <admin@Mac-Admin.local>
2017-06-26 14:39:07 +0300

    Initial project

---
+++ GiphyApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GiphyApp
+//
+//  Created by Pavel Samsonov on 26/06/2017.
+//  Copyright © 2017 Pavel Samsonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
